e6a800ef777c6ae25b3dc7f5b04b3421522cd51d
hoangtrung1002 <n.jordan1002@gmail.com>
2025-09-16 23:03:54 +0700

    styles: customize styling components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="p-5">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am a button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am input" />
+        </div>
+        <div>
+          <Progress value={33} />
+        </div>
+      </div>
+    </div>
+  );
 }
