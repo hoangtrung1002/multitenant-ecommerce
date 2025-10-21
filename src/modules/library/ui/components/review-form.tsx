c8f4d47897b85584590208c29fa3542d413f0dc1
hoangtrung1002 <n.jordan1002@gmail.com>
2025-10-21 22:46:50 +0700

    fix: add error display, richtext to product'

--- src/modules/library/ui/components/review-form.tsx
+++ src/modules/library/ui/components/review-form.tsx
@@ -147,3 +147,23 @@ const ReviewForm = ({ initialData, productId }: Props) => {
 };
 
 export default ReviewForm;
+
+export const ReviewFormSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <p className="font-medium">Liked it? Give it a rating</p>
+      <StarPicker disabled />
+      <Textarea placeholder="Want to leave a written?" disabled />
+
+      <Button
+        variant="elevated"
+        disabled
+        type="button"
+        size="lg"
+        className="text-white bg-black hover:bg-pink-400 hover:text-primary w-fit"
+      >
+        Post review
+      </Button>
+    </div>
+  );
+};
