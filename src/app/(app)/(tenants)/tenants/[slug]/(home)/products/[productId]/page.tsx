f9b018c983a01184969ae454f244334d4e0bf0da
hoangtrung1002 <n.jordan1002@gmail.com>
2025-10-23 21:12:45 +0700

    deployment

--- src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
+++ src/app/(app)/(tenants)/tenants/[slug]/(home)/products/[productId]/page.tsx
@@ -9,9 +9,10 @@ interface Props {
   params: Promise<{ productId: string; slug: string }>;
 }
 
+export const dynamic = "force-dynamic";
+
 const Page = async ({ params }: Props) => {
   const { productId, slug } = await params;
-
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.tenants.getOne.queryOptions({ slug }));
 
